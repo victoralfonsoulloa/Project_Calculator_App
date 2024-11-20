fe5c0f4a17e3fb6f483e99dad3d4c8be66028b89
Victor Ulloa <victoralfonsoulloa@gmail.com>
2024-11-19 23:20:43 -0700

    Define function for event handler on click

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ const resultDiv = document.getElementById('result');
 let expression = ' ';
 let result = ' ';
 
+//Define event handler for button clicks
+function buttonClick(event) {
+    //Get values from clicked button
+    const target = event.target;
+    const action = target.dataset.action;
+    const value = target.dataset.value;
+    //console.log(target, action, value)
+  
+    //Switch case to control the calculator
+    switch (action) {
+      case 'number':
+        console.log('test');
+        break;
+      case 'number':
+        console.log('test');
+        break;
+    }
+  }
+  
+  inputBox.addEventListener('click', buttonClick);
+  

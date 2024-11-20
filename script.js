e7d386cd43ddfffd11edaeffb010373c2490820a
Victor Ulloa <victoralfonsoulloa@gmail.com>
2024-11-19 23:57:15 -0700

    Add cases for different arithmethic operations

--- script.js
+++ script.js
@@ -3,54 +3,59 @@ const inputBox = document.getElementById('input');
 const expressionDiv = document.getElementById('expression');
 const resultDiv = document.getElementById('result');
 
-//Define expression and result variable
+// Define expression and result variable
 let expression = ' ';
 let result = ' ';
 
-//Define event handler for button clicks
+// Define event handler for button clicks
 function buttonClick(event) {
-    //Get values from clicked button
-    const target = event.target;
-    const action = target.dataset.action;
-    const value = target.dataset.value;
-    //console.log(target, action, value)
-  
-    //Switch case to control the calculator
-    switch (action) {
-      case 'number':
-        addValue(value);
-        break;
-      case 'clear':
-        clear();
-        break;
-      case 'backspace':
-        backspace();
-        break;
-    }
+  //Get values from clicked button
+  const target = event.target;
+  const action = target.dataset.action;
+  const value = target.dataset.value;
+  //console.log(target, action, value)
 
-    //Update display
-    updateDisplay(expression, result)
-  }
-  
-  inputBox.addEventListener('click', buttonClick);
-  
-
-  function addValue(value) {
-    //Add value to the expression 
-    expression += value;
-    //console.log(expression)
+  // Switch case to control the calculator
+  switch (action) {
+    case 'number':
+      addValue(value);
+      break;
+    case 'clear':
+      clear();
+      break;
+    case 'backspace':
+      backspace();
+      break;
+    // Add the result to the expression as a starting point if expression is empty
+    case 'addition':
+    case 'subtraction':
+    case 'multiplication':
+    case 'division':
+    
   }
 
-  function updateDisplay(expression, rsult) {
-    expressionDiv.textContent = expression;
-    resultDiv.textContent = result;
-  }
+  // Update display
+  updateDisplay(expression, result);
+}
 
-  function clear() {
-    expression = ' ';
-    result = ' ';
-  }
+inputBox.addEventListener('click', buttonClick);
 
-  function backspace() {
-    expression = expression.slice(0, -1)
-  }
+function addValue(value) {
+  // Add value to the expression
+  expression += value;
+  //console.log(expression)
+}
+
+function updateDisplay(expression, result) {
+  expressionDiv.textContent = expression;
+  resultDiv.textContent = result;
+}
+
+function clear() {
+  expression = ' ';
+  result = ' ';
+}
+
+function backspace() {
+  expression = expression.slice(0, -1);
+}

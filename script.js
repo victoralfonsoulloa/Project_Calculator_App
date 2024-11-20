e75e0681e315f01d7e370ac51d3c13e30cb93516
Victor Ulloa <victoralfonsoulloa@gmail.com>
2024-11-19 23:19:31 -0700

    Access DOM elements of the calculator

---
+++ script.js
@@ -0,0 +1,9 @@
+// Access DOM elements of the calculator
+const inputBox = document.getElementById('input');
+const expressionDiv = document.getElementById('expression');
+const resultDiv = document.getElementById('result');
+
+//Define expression and result variable
+let expression = ' ';
+let result = ' ';
+
